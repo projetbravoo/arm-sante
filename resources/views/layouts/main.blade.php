dd8ec45147704d2d77a61e75c7557122a14f5329
Salam Sahane <salamsahane@gmail.com>
2021-12-10 15:11:55 +0100

    Intergration of the template

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,37 @@
+@include('layouts.partials._header')
+
+<div class="main-wrapper">
+
+    @include('layouts.partials._navbar')
+
+
+    {{-- <div class="breadcrumb-bar">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-md-12 col-12">
+                    <nav aria-label="breadcrumb" class="page-breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Blank Page</li>
+                        </ol>
+                    </nav>
+                    <h2 class="breadcrumb-title">Blank Page</h2>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+
+
+    <div class="content" style="min-height:67px;">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+@include('layouts.partials._footer')
